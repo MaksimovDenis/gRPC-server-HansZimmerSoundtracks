5d85597739b8d838dcd4f165cef5e6d45dbe3038
Denis <maksimovis74@gmail.com>
2024-02-21 15:57:24 +0500

    comment has been added

--- sso/cmd/sso/main.go
+++ sso/cmd/sso/main.go
@@ -27,7 +27,7 @@ func main() {
 
 	log.Info("starting application")
 
-	application := app.New(log, cfg.GRPC.Port, cfg.StoragePath, cfg.TokenTTL)
+	application := app.New(log, cfg.GRPC.Port, cfg.StoragePath, cfg.TokenTTL) //General
 
 	go application.GRPCSrv.MustRun()
 
